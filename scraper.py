eaa902450f57de17285d5d1ce2fcb50afa2a7add
Billy Kwong <tuppa@tuppa.org>
2020-06-22 00:06:56 +1000

    Initial commit

--- scraper.py
+++ scraper.py
@@ -1,24 +1,56 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import datetime
+from bs4 import BeautifulSoup
+from requests import get
+import sqlite3
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+# Georges River Council eTrack applications submitted this month
+url = "https://etrack.georgesriver.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx?d=thismonth&k=LodgementDate&"
+
+#
+# Read in the page and parse it with BS4
+soup = BeautifulSoup(get(url).text, 'html.parser')
+results = soup.find(id="hiddenresult")
+
+# Write out to the sqlite database using scraperwiki library
+# Required fields: https://www.planningalerts.org.au/how_to_write_a_scraper
+# SQlite documentation: https://morph.io/documentation
+sqlite = sqlite3.connect('data.sqlite')
+with sqlite:
+    sqlite.execute('''
+        create table data (
+            council_reference varchar unique,
+            address text,
+            info_url text,
+            description text
+            )
+    ''')
+
+    # Scrape the application list
+    for entry in results('tr'):
+        print("---")
+        print(entry)
+        print("---")
+        council_reference = entry.find(class_="col2").div.text
+        address = entry.find(class_="col3").text.strip()
+        info_url = entry.a.get('href')
+        description = entry.find(class_="col3").text.split('\n').pop()
+        date_scraped = datetime.date.today().strftime("%Y-%m-%d")
+
+
+        sqlite.execute(f'''
+            insert into data (
+                council_reference,
+                address,
+                info_url,
+                description)
+
+            values (
+                "{council_reference}",
+                "{description}",
+                "{info_url}",
+                "{date_scraped}"
+            )
+        ''')
